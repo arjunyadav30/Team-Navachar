5f746feb7f6f2f9a8ae7c0208bbd4024f42d3f17
arjunyadav30 <mr.aj.yadav9644@gmail.com>
2025-08-28 15:15:12 +0530

    Update index.js

--- Day 5/index.js
+++ Day 5/index.js
@@ -1,1 +1,30 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+
+app.use(express.json());
+
+let students = [
+  { id: 1, name: "Arjun" },
+  { id: 2, name: "Prachi" },
+  { id: 3, name: "Aradhy" }
+];
+
+app.get("/students", (req, res) => {
+  res.json(students);
+});
+
+app.post("/students", (req, res) => {
+  let newStudent = {
+    id: students.length + 1,
+    name: req.body.name
+  };
+  students.push(newStudent);
+  res.json(newStudent);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}/students`);
+});
 
